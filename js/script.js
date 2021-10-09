4e7cd62e15d95ed60fe1a6b19fcf2e44e5fe0784
IsraelSenpai <raelfaustino9@gmail.com>
2021-10-08 22:33:40 -0300

    Slidefolio

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//
+//	jQuery Validate example script
+//
+//	Prepared by David Cochran
+//
+//	Free for your use -- No warranties, no guarantees!
+//
+
+$(document).ready(function(){
+
+
+
+		$('#contact-form').validate({
+	    rules: {
+	      name: {
+	        minlength: 2,
+	        required: true
+	      },
+	      email: {
+	        required: true,
+	        email: true
+	      },
+	      subject: {
+	      	minlength: 2,
+	        required: true
+	      },
+	      message: {
+	        minlength: 2,
+	        required: true
+	      }
+	    },
+			highlight: function(element) {
+				$(element).closest('.form-group').removeClass('success').addClass('error');
+			},
+			success: function(element) {
+				element
+				.text('OK!').addClass('valid')
+				.closest('.form-group').removeClass('error').addClass('success');
+			}
+	  });
+
+}); // end document.ready
